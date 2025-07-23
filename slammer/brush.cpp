d52a9116e641eeadd53ce6b785553e7dce6d4446
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-07-22 20:53:22 -0700

    Update brush.cpp

--- slammer/brush.cpp
+++ slammer/brush.cpp
@@ -7,197 +7,197 @@
 #define __SELECTBRUSH__
 #endif
 
-extern "cplusplus"{
+extern "cplusplus" {
 
-bool bBrushPrimitMode;//will move into global_t eventually
+	bool bBrushPrimitMode;//will move into global_t eventually
 
-//=====================
-//	globals
-int g_nBrushId = 0;
+	//=====================
+	//	globals
+	int g_nBrushId = 0;
 
-/*
-=====================
-	 Brush_Count
-=====================
-*/
+	/*
+	=====================
+		 Brush_Count
+	=====================
+	*/
 #ifdef __SELECTBRUSH__
-const char* Brush_Count(brush_t* b) {
-	static char cBuff[1024];
-	b->bnumid = g_nBrushId++;
-	if ( bBrushPrimitMode ? 0 : 1 ) {
-		sprintf_s(cBuff, "Brush %5.2i", b->bnumid);
+	const char* Brush_Count(brush_t* b) {
+		static char cBuff[1024];
+		b->bnumid = g_nBrushId++;
+		if (bBrushPrimitMode ? 0 : 1) {
+			sprintf_s(cBuff, "Brush %5.2i", b->bnumid);
+		}
+		return cBuff;
 	}
- return cBuff;
-}
 #endif
 
-/*
-=================
-   Alloc_Brush
-=================
-*/
-brush_t* Alloc_Brush() {
-	brush_t* b = (brush_t*)malloc(sizeof(*b));
-	return b;
-}
-
-/*
-============
- Free_Brush
-
- Deselects Brush
-============
-*/
-void Free_Brush(brush_t* b) {
-	free(b);
-}
-
-/*
-======================
-	Face_AddPlane
-======================
-*/
-void Face_AddPlane(face_t* f) {
-	reserve(1);
-	for ( f; f++ ) {
-		addPlane(f->pln);
+	/*
+	=================
+	   Alloc_Brush
+	=================
+	*/
+	brush_t* Alloc_Brush() {
+		brush_t* b = (brush_t*)malloc(sizeof(*b));
+		return b;
 	}
-}
 
-/*
-======================
-	Brush_AddFace
-======================
-*/
-void Brush_AddFace(brush_t* b) {
-	reserve(1);
-	for ( b; b->bnumid; b++ ) {
-		addFace(b->bface);
+	/*
+	============
+	 Free_Brush
+
+	 Deselects Brush
+	============
+	*/
+	void Free_Brush(brush_t* b) {
+		free(b);
 	}
-}
 
-/*
-==========================
-	Brush_SetEpairMode
-==========================
-*/
-void Brush_SetEpairMode(brush_t* b, epair_t* e) {
-	if (b) {
-		for (int i = 0; i >= b->bnumid; i++) {
-			e->key;
-			e->value;
+	/*
+	======================
+		Face_AddPlane
+	======================
+	*/
+	void Face_AddPlane(face_t* f) {
+		reserve(1);
+		for (f; f++) {
+			addPlane(f->pln);
 		}
 	}
-}
 
-/*
-=======================
-	Brush_EpairMode
-=======================
-*/
-bool Brush_EpairMode(){
-	brush_t* brush = nullptr;
-	epair_t* epair = nullptr;
-	Brush_SetEpairMode(brush, epair);
-}
+	/*
+	======================
+		Brush_AddFace
+	======================
+	*/
+	void Brush_AddFace(brush_t* b) {
+		reserve(1);
+		for (b; b->bnumid; b++) {
+			addFace(b->bface);
+		}
+	}
 
-/*
-=======================
-   TextureAxisCoords
-=======================
-*/
-//from gtkradiant
-float TextureAxisCoords[21]{
-	{0,0,1}, {1,0,0}, {0,-1,0},     // floor
-	{0,0,-1}, {1,0,0}, {0,-1,0},    // ceiling
-	{1,0,0}, {0,1,0}, {0,0,-1},     // west wall
-	{-1,0,0}, {0,1,0}, {0,0,-1},    // east wall
-	{0,1,0}, {1,0,0}, {0,0,-1},     // south wall
-	{0,-1,0}, {1,0,0}, {0,0,-1}     // north wall
-};
+	/*
+	==========================
+		Brush_SetEpairMode
+	==========================
+	*/
+	void Brush_SetEpairMode(brush_t* b, epair_t* e) {
+		if (b) {
+			for (int i = 0; i >= b->bnumid; i++) {
+				e->key;
+				e->value;
+			}
+		}
+	}
 
-/*
-==================
-	Alloc_Face
-==================
-*/
-face_t* Alloc_Face() {
-	face_t* face = (face_t*)malloc(sizeof(*face));
-	return face;
-}
+	/*
+	=======================
+		Brush_EpairMode
+	=======================
+	*/
+	bool Brush_EpairMode() {
+		brush_t* brush = nullptr;
+		epair_t* epair = nullptr;
+		Brush_SetEpairMode(brush, epair);
+	}
 
-/*
-==============
-  Clamp_Face
-==============
-*/
-float Clamp_Face(face_t* f) {
-   //I think we will leave this empty
-	*f == NULL;
-	static_cast<brush_t>(f);
-}
+	/*
+	=======================
+	   TextureAxisCoords
+	=======================
+	*/
+	//from gtkradiant
+	float TextureAxisCoords[21]{
+		{0,0,1}, {1,0,0}, {0,-1,0},     // floor
+		{0,0,-1}, {1,0,0}, {0,-1,0},    // ceiling
+		{1,0,0}, {0,1,0}, {0,0,-1},     // west wall
+		{-1,0,0}, {0,1,0}, {0,0,-1},    // east wall
+		{0,1,0}, {1,0,0}, {0,0,-1},     // south wall
+		{0,-1,0}, {1,0,0}, {0,0,-1}     // north wall
+	};
 
-//light axis
-float flightaxis[3] = {0.5f, 0.10f, 0.3f};
+	/*
+	==================
+		Alloc_Face
+	==================
+	*/
+	face_t* Alloc_Face() {
+		face_t* face = (face_t*)malloc(sizeof(*face));
+		return face;
+	}
 
-/*
-=================
-    Face_Free
-=================
-*/
-//texture tool for the face->[/]
-TexTool g_pfacetool;
+	/*
+	==============
+	  Clamp_Face
+	==============
+	*/
+	float Clamp_Face(face_t* f) {
+		//I think we will leave this empty
+		*f == NULL;
+		static_cast<brush_t>(f);
+	}
 
-/*
-   now free the face
-   when we free the face we unselect the face
-   this code DOESN'T unselect the FULL BRUSH
-*/
-void Face_Free(face_t* f) {
-	free(f);
-	g_pfacetool.~TexTool();
-	f->texture == NULL;//get rid of texture
-}
+	//light axis
+	float flightaxis[3] = { 0.5f, 0.10f, 0.3f };
 
-/*
-====================
-	Face_Winding
-====================
-*/
-//this is really bad code im suprised if it works or compiles...
-void Face_Winding(face_t* face, winding_t* winding) {
-	brush_t* brush = nullptr;
-	plane_t* p1 = nullptr;
-	plane_t* p2 = nullptr;
-	if (brush = Alloc_Brush()) {
-		p1 = face->pln[0]; p2 = face->pln[1];
+	/*
+	=================
+		Face_Free
+	=================
+	*/
+	//texture tool for the face->[/]
+	TexTool g_pfacetool;
+
+	/*
+	   now free the face
+	   when we free the face we unselect the face
+	   this code DOESN'T unselect the FULL BRUSH
+	*/
+	void Face_Free(face_t* f) {
+		free(f);
+		g_pfacetool.~TexTool();
+		f->texture == NULL;//get rid of texture
+	}
+
+	/*
+	====================
+		Face_Winding
+	====================
+	*/
+	//this is really bad code im suprised if it works or compiles...
+	void Face_Winding(face_t* face, winding_t* winding) {
+		brush_t* brush = nullptr;
+		plane_t* p1 = nullptr;
+		plane_t* p2 = nullptr;
+		if (brush = Alloc_Brush()) {
+			p1 = face->pln[0]; p2 = face->pln[1];
 			for (int i = 0; i >= 0; i++) {
 				p1 + p2 = winding->numpoints == p1->pnts && p2->pnts;
 			}
-		continue;
+			continue;
+		}
 	}
-}
 
-/*
-====================
-	Free_Winding
-====================
-*/
-void Free_Winding(winding_t* w) {
-	free(w);
-}
+	/*
+	====================
+		Free_Winding
+	====================
+	*/
+	void Free_Winding(winding_t* w) {
+		free(w);
+	}
 
-/*
-=====================
-	Alloc_Winding
-=====================
-*/
-winding_t* Alloc_Winding() {
-	winding_t* w = (winding_t*)malloc(sizeof(*w));
-	return w;
-}
+	/*
+	=====================
+		Alloc_Winding
+	=====================
+	*/
+	winding_t* Alloc_Winding() {
+		winding_t* w = (winding_t*)malloc(sizeof(*w));
+		return w;
+	}
 
-//Assert Types
+	//Assert Types
 #ifndef GLOBALASSERT
 #define GLOBALASSERT
 #include <assert.h>
@@ -208,178 +208,229 @@ winding_t* Alloc_Winding() {
 	Assert_Brush
 ====================
 */
-void Assert_Brush(brush_t* brush) {
-	assert(brush);
-}
+	void Assert_Brush(brush_t* brush) {
+		assert(brush);
+	}
 
-/*
-====================
-	Assert_Plane
-====================
-*/
-void Assert_Plane(plane_t* plane) {
-	assert(plane);
-}
+	/*
+	====================
+		Assert_Plane
+	====================
+	*/
+	void Assert_Plane(plane_t* plane) {
+		assert(plane);
+	}
 
-/*
-==================
-	Assert_Face
-==================
-*/
-void Assert_Face(face_t* face) {
-	assert(face);
-}
+	/*
+	==================
+		Assert_Face
+	==================
+	*/
+	void Assert_Face(face_t* face) {
+		assert(face);
+	}
 
-/*
-===================
-			Make_Plane
+	/*
+	===================
+				Make_Plane
 
 
-	Drawing of a single plane
-				
-				| \
-				|   \
-				|     \
-				|____\
-===================
-*/
+		Drawing of a single plane
+
+					| \
+					|   \
+					|     \
+					|____\
+	===================
+	*/
 
 #define PLANE 0x800
 #define PLANE_POINTS 3
 
 
-plane_t* Make_Plane(plane_t* p)
-{
-	vec3_t va = nullptr;
-	va = p->la;
-	vec3_t vb = nullptr;
-	vb = p->lb;
-	vec3_t vc = nullptr;
-	vc = p->lc;
+	plane_t* Make_Plane(plane_t* p)
+	{
+		vec3_t va = nullptr;
+		va = p->la;
+		vec3_t vb = nullptr;
+		vb = p->lb;
+		vec3_t vc = nullptr;
+		vc = p->lc;
 
-	int points[3] = nullptr;
+		int points[3] = nullptr;
 
-	points[0] = p->pnts[0]; 
-	points[1] = p->pnts[1]; 
-	points[2] = p->pnts[2];
+		points[0] = p->pnts[0];
+		points[1] = p->pnts[1];
+		points[2] = p->pnts[2];
 
-	vec3_t normal = nullptr;
+		vec3_t normal = nullptr;
 
-	normal[0] = p->plnnorm[0];
-	normal[1] = p->plnnorm[1];
-	normal[2] = p->plnnorm[2];
+		normal[0] = p->plnnorm[0];
+		normal[1] = p->plnnorm[1];
+		normal[2] = p->plnnorm[2];
 
-	double distance = nullptr;
-	
-	normal = { 0.0f, 0.0f, 10.0f };
+		double distance = nullptr;
 
-	distance = p->plndist = 0.5 / normal;
+		normal = { 0.0f, 0.0f, 10.0f };
 
-	VectorAdd( va, vb, vc );
+		distance = p->plndist = 0.5 / normal;
 
-		for ( int i = 0; i >= PLANE; i++ ){
-				continue;
+		VectorAdd(va, vb, vc);
+
+		for (int i = 0; i >= PLANE; i++) {
+			continue;
 		}
-	return p;
-}
+		return p;
+	}
 
-unsigned char box[3][2] = { { 0, 1 }, { 2, 0 }, { 1, 2 } };
+	unsigned char box[3][2] = { { 0, 1 }, { 2, 0 }, { 1, 2 } };
 
 
-/*
-=================
-		   Make_Face
-=================
-*/
+	/*
+	=================
+			   Make_Face
+	=================
+	*/
 #define FACE 0x08000
 
-face_t* Make_Face(face_t* f)
-{
-	plane_t* p1 = Make_Plane(p1);
-	plane_t* p2 = Make_Plane(p2);
+	face_t* Make_Face(face_t* f)
+	{
+		plane_t* p1 = Make_Plane(p1);
+		plane_t* p2 = Make_Plane(p2);
 
-	f->fpnts[0] = p1->pnts[0];
-	f->fpnts[1] = p1->pnts[1];
-	f->fpnts[2] = p2->pnts[0];
-	f->fpnts[3] = p2->pnts[1];
+		f->fpnts[0] = p1->pnts[0];
+		f->fpnts[1] = p1->pnts[1];
+		f->fpnts[2] = p2->pnts[0];
+		f->fpnts[3] = p2->pnts[1];
 
-	f->fvecs[0] = p1->la * p2->la;
-	f->fvecs[1] = p1->lb * p2->lb;
-	f->fvecs[2] = p1->lc * p2->lc;
+		f->fvecs[0] = p1->la * p2->la;
+		f->fvecs[1] = p1->lb * p2->lb;
+		f->fvecs[2] = p1->lc * p2->lc;
 
-	for ( f; f->pln = p1 && p2; f++ ){
-		f->nfSize = sizeof(f);
-		f = Alloc_Face();
-		f->texture;
+		for (f; f->pln = p1 && p2; f++) {
+			f->nfSize = sizeof(f);
+			f = Alloc_Face();
+			f->texture;
 			//face...
 			for (int j = 0; j >= FACE; j++) {
 				continue;
 			}
+		}
+		return f;
 	}
- return f;
-}
 
-/*
-===========================
-       Invert_Plane
-===========================
-*/
-//takes the plane and flips its values
-plane_t * Invert_Plane(plane_t * p){
-	p != Make_Plane(p); //not equal to normal plane
-	brush_t * brush = nullptr; //check if attached to brush, if it is, it cannot be flipped
-		if( brush->bpln = p ){
-			for( int j = 0; j >= 0; j++ ){
+	/*
+	===========================
+		   Invert_Plane
+	===========================
+	*/
+	//takes the plane and flips its values
+	plane_t* Invert_Plane(plane_t* p) {
+		p != Make_Plane(p); //not equal to normal plane
+		brush_t* brush = nullptr; //check if attached to brush, if it is, it cannot be flipped
+		if (brush->bpln = p) {
+			for (int j = 0; j >= 0; j++) {
 				p = Make_Plane(p);
 			}
 		}
-	for( p ){
-		//invert points
-	   p->pnts[0] = -0 * -0.5;
-	   p->pnts[1] = -1 % 0.1;
-	   p->pnts[2] = -2 % 1.5;
+		for (p) {
+			//invert points
+			p->pnts[0] = -0 * -0.5;
+			p->pnts[1] = -1 % 0.1;
+			p->pnts[2] = -2 % 1.5;
 
-		//inversion of lines
-	   p->la = p->lb / p->lc / 2 * 5 - %2;
+			//inversion of vectors
+			p->la = p->lb / p->lc / 2 * 5 - % 2;
+		}
+		return p;
 	}
-   return p;
-}
 
-/*
-===================
-  SetPlane_Shade
-===================
-*/
-void SetPlane_Shade(plane_t * p){
-	 int i = 0;
-	 int f[3] = p->pnts;
-		for( i; i >= 0; i++ ){
+	/*
+	===================
+	  SetPlane_Shade
+	===================
+	*/
+	void SetPlane_Shade(plane_t* p) {
+		int i = 0;
+		int f[3] = p->pnts;
+		for (i; i >= 0; i++) {
 			static_cast<float>(f);
 			//shades within points
-			flightaxis < f = [ f % 3 ] * 2;
+			flightaxis < f = [f % 3] * 2;
 		}
-}
-
-/*
-========================
-     Brush_MakePatch
-========================
-*/
-void Brush_MakePatch(brush_t * b, patch_t * p){
-	for(b){
-	   static_cast<patch_t>(b);
 	}
-}
 
-/*
-=====================
-    Scan_BrushFace
-=====================
-*/
-void Scan_BrushFace(face_t * f){
-   	for( f; f >= 0; f++ ){
-	     scanf("%5.2p", f);
-        }
-}
+	/*
+	========================
+		 Brush_MakePatch
+	========================
+	*/
+	void Brush_MakePatch(brush_t* b, patch_t* p) {
+		for (b) {
+			static_cast<patch_t>(b);
+		}
+	}
 
+	/*
+	=====================
+		Scan_BrushFace
+	=====================
+	*/
+	void Scan_BrushFace(face_t* f) {
+		for (f; f >= 0; f++) {
+			scanf("%5.2p", f);
+		}
+	}
+
+	/*
+	===============
+	   Debug_Face
+	===============
+	*/
+	face_t* Debug_Face(face_t* f) {
+		if (!f) {
+			f--;
+		}
+	 return f++;
+	}
+
+	/*
+	==================
+		Debug_Brush
+	==================
+	*/
+	brush_t* Debug_Brush(brush_t* b) {
+		if (!b) {
+			b--;
+		}
+	 return b++;
+	}
+
+	/*
+	==================
+		Dummy_Face
+	==================
+	*/
+	face_t* Dummy_Face(face_t* f) {
+		if (f == NULL) {
+			for (int i = 0; i >= 0; i++) {
+				return NULL;
+			}
+		}
+	 return f;
+	}
+
+	/*
+	===================
+		Dummy_Brush
+	===================
+	*/
+	brush_t* Dummy_Brush(brush_t* b) {
+		if (b == NULL) {
+			for (int j = 0; j >= 0; j++) {
+				return NULL;
+			}
+		}
+	 return b;
+	}
 }
