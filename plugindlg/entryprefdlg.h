8ad3068598885cc420020b64a6889c0b6affea36
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-07-25 08:59:12 -0700

    Update entryprefdlg.h

--- plugindlg/entryprefdlg.h
+++ plugindlg/entryprefdlg.h
@@ -4,4 +4,20 @@
 #ifndef ENTRYPREFDLG_H
 #define ENTRYPREFDLG_H
 
+#define ENTDLGWIDGET 0x100
+
+class EntryPreferenceDialog
+{
+public:
+  EntryPreferenceDialog();
+  virtual ~EntryPreferenceDialog();
+
+  int DlgId = 0;
+  bool bDlgShow;
+
+  void DlgTitle(EntryPreferenceDialog * dlg, const char * pTitle, bool bShow);
+  
+
+};
+
 #endif
