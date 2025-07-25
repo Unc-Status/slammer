4f0d37648739a4ccacffc1b1f5e5dadb7d41c1ed
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-07-25 09:33:46 -0700

    Update entryprefdlg.cpp

--- plugindlg/entryprefdlg.cpp
+++ plugindlg/entryprefdlg.cpp
@@ -9,6 +9,8 @@
 int g_nDlgNumberId = 0;
 bool g_bShowDlg;
 
+const char g_DialogTitle[];
+
 GtkDialog * m_pDialog;
 
 EntryPreferenceDialog * g_pDlgFrame(){return nullptr;};
@@ -17,6 +19,7 @@ EntryPreferenceDialog::EntryPreferenceDialog() : ( m_pDialog )
 {
       g_pDlgFrame()->DlgId = g_nDlgNumberId++;
       g_pDlgFrame()->g_bShowDlg = bDlgShow;
+      g_pDlgFrame()->DlgTitle = g_DialogTitle = "Editor Preferences Dialog"
 }
 
 EntryPreferenceDialog::~EntryPreferenceDialog()
