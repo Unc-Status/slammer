22700a000d6ac464c1f40d067a0bf24c992ed9b4
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-07-25 13:03:20 -0500

    Update and rename e.txt to etypes.h

---
+++ econtrib/etypes.h
@@ -0,0 +1,19 @@
+//etypes.h
+//praise the Lord for my code!
+
+#ifndef __ETYPES_H__
+#define __ETYPES_H__
+
+typedef float efloat;
+typedef int eint;
+typedef double edouble;
+typedef bool eboolean;
+typedef true etrue;
+typedef false efalse;
+typedef char echar;
+
+typedef struct etype_s{
+   const echar* keyword[]{return "";};
+}etype_t;
+
+#endif
