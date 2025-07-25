a2fe88d6bf24e9d699ef5de504fdcce0bcdc410d
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-07-24 19:59:52 -0700

    Update entityw.h

--- entitywad/entityw.h
+++ entitywad/entityw.h
@@ -12,6 +12,16 @@ public:
   virtual ~EntityW();
 
   int entityid = nullptr;
+  int entitysize = sizeof(EntityW);
+
+  bool entityspawned = nullptr;
+
+  void EntityEpair(EntityW * e, const char * key, const char * value);
+  void ConnectEntities(EntityW * e1, EntityW * e2);
+  void EntityFree(EntityW * e);
+  void AllocEntity();
+  void EntityName(EntityW * e);
+  void DrawAABBOnEntity(EntityW * e, float mins, float maxs, bool bSpawned);
 
 };
 
