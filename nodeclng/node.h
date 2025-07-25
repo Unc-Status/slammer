859138d3bdb64034a63bbf73a7cbdbdd06539498
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-07-24 20:54:02 -0700

    Update node.h

--- nodeclng/node.h
+++ nodeclng/node.h
@@ -1,1 +1,28 @@
+//node.h
+//praise the Lord, he is the true author of my code
 
+#ifndef __NODE_H__
+#define __NODE_H__
+
+#include "mathlib.h"
+
+class Node {
+public:
+  Node();
+  virtual ~Node();
+
+  int NodeId = 0;
+  int NodeType = nullptr;
+
+  int NodeCount = 0;
+
+  vec3_t NodePosition;
+
+  bool bNodeReset;
+
+};
+
+//pointer
+Node NodePtr;
+Node * Nde;
+#endif
