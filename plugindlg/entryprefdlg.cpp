6efd0f4fc68c9d0e55074708a32d65ddc6f0a450
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-07-25 09:27:58 -0700

    Create entryprefdlg.cpp

---
+++ plugindlg/entryprefdlg.cpp
@@ -0,0 +1,20 @@
+//entryprefdlg.cpp
+//praise the Lord for my code!
+
+#include "entryprefdlg.h"
+
+#ifndef __GTKWIDGET_H__
+#define __GTKWIDGET_H__
+
+int g_nDlgNumberId = 0;
+bool g_bShowDlg;
+
+GtkDialog * m_pDialog;
+
+EntryPreferenceDialog::EntryPreferenceDialog() : (m_pDialog)
+{
+
+};
+
+
+#endif
