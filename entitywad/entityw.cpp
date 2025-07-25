42504858dc25e154d5071cc5324b845d6b7f2cc2
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-07-24 20:21:41 -0700

    Update entityw.cpp

--- entitywad/entityw.cpp
+++ entitywad/entityw.cpp
@@ -2,6 +2,7 @@
 //praise the Lord, he is the true author of my code
 
 #include "entityw.h"
+#include "slammerplugins.h"
 
 int g_EntityId = 0;
 bool g_EntitySpawned;
@@ -22,4 +23,9 @@ EntityW::~EntityW()
 void EntityW::AllocEntity()
 {
     EntityW * e = (EntityW*)malloc(sizeof(EntityW));
+}
+
+void EntityW::EntityEpair(EntityW * e, const char * key, const char * value)
+{
+    
 }
