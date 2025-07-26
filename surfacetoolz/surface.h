52b5772062f4f997023abb684bb7868a229d6d6e
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-07-26 19:19:48 -0400

    Update surface.h

--- surfacetoolz/surface.h
+++ surfacetoolz/surface.h
@@ -4,6 +4,21 @@
 #ifndef SURFACE_H
 #define SURFACE_H
 
+/*
+    Surfaces cannot be selected or have a texture applied to them
+
+    possible pointers:
+    - surface_t* s; commonly used
+    - surface_t* srf;
+    - surface_t* sface; if converted to a face
+    - surface_t* pSrf;
+    - surface_t* surface;
+
+    elements:
+    - points[4]
+    - bSelectable
+*/
+
 #include "mathlib.h"
 
 typedef struct surface_s{
@@ -13,8 +28,8 @@ typedef struct surface_s prev;//use &
 typedef struct surface_s ** next;
 
 float points[4];
-bool bSelectable;
-    //winding_t* surfacewinding;
+bool bSelectable;//shouldnt be true, if it is its automatically converted to a face
+
 }surface_t;
 
 #endif
