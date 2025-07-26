6dd9f113a94056907901259a27497f696a2beefa
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-07-26 16:51:31 -0400

    Update reversepointer.h

--- modstax/reversepointer.h
+++ modstax/reversepointer.h
@@ -1,5 +1,7 @@
 //reversepointer.h
 //Praise be to my Lord for my code!!!
+//Jesus Christ Is King
+//He who believes in him shall not perish but have eternal life!
 
 #ifndef __REVERSE_POINTER_H__
 #define __REVERSE_POINTER_H__
@@ -11,9 +13,36 @@ const char * code[];
 const char * ReverseInt(){
     const char* _type = "<-int";
     if( code == _type ){
-       return "<-int";
+       return "";
     }
-  return *code[_type];
+    return *code[*_type];
+}
+
+//reverse float pointer
+const char* ReverseFloat(){
+    const char* _type = "<-float";
+    if( code == _type ){
+        return "";
+    }
+    return *code[*_type];
+}
+
+//reverse bool pointer
+const char* ReverseBool(){
+    const char* _type = "<-bool";
+    if( code == _type ){
+        return "";
+    }
+    return *code[*_type];
+}
+
+//reverse double pointer
+const char* ReverseDouble(){
+    const char* _type = "<-double";
+    if( code == _type ){
+        return "";
+    }
+    return *code[*_type];
 }
 
 #endif
