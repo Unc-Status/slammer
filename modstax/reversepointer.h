d7ffc67a90d7a4e712b91dbcedc0edfa85ad4bb9
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-07-26 16:28:20 -0400

    Create reversepointer.h

---
+++ modstax/reversepointer.h
@@ -0,0 +1,19 @@
+//reversepointer.h
+//Praise be to my Lord for my code!!!
+
+#ifndef __REVERSE_POINTER_H__
+#define __REVERSE_POINTER_H__
+
+//search for key pointers
+const char * code[];
+
+//reverse int pointer
+const char * ReverseInt(){
+    const char* _type = "<-int";
+    if( code == _type ){
+       return "<-int";
+    }
+  return *code[_type];
+}
+
+#endif
