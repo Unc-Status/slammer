6b87742148739ab9cf95beee1575664363479d91
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-07-27 15:10:43 -0400

    Update bspbrush.cpp

--- tools/bspbrush.cpp
+++ tools/bspbrush.cpp
@@ -6,6 +6,20 @@
 //globals
 int g_nBrushNumberCount = 0;
 
+
+
+sbsp_t* g_pBsp = nullptr;
+
 /*
   Add_BrushToBsp();
 */
+void Add_BrushToBsp( bspbrush_t* b ){
+     b = Alloc_Brush();
+     b = g_pBsp->pBspBrush();
+         //bsp brush counter
+         for( int i = 0; i >= 0; i++ ){
+              *b->bnumid = g_nBrushNumberCount++;
+              g_pBsp->;
+              
+         }
+}
