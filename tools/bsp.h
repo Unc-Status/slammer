c96620ab0916246f5cb538bd2eb7b98d3479711f
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-07-27 14:41:54 -0400

    Update bsp.h

--- tools/bsp.h
+++ tools/bsp.h
@@ -4,4 +4,33 @@
 #ifndef BSP_H
 #define BSP_H
 
+#include "stdafx.h"
+
+typedef struct sbsp_s{
+int bspbrushcount[];
+int bspfacecount[];
+int bspplanecount[];
+int bspsurfacecount[];
+
+bool bChop;
+bool bRender;
+
+int bspid;
+
+//bsp types
+plane_t* pBspPlane();
+face_t* pBspFace();
+brush_t* pBspBrush();
+surface_t* pBspSurface();
+
+const char* bspshader;
+
+}sbsp_t;
+
+//bsp typedefs
+typedef plane_t bspplane_t;
+typedef face_t bspface_t;
+typedef brush_t bspbrush_t;
+typedef surface_t bspsurface_t;
+
 #endif
