8f82361f631e53162e3a365aa4460d246247366d
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-07-26 20:36:27 -0400

    Update surface.cpp

--- surfacetoolz/surface.cpp
+++ surfacetoolz/surface.cpp
@@ -13,6 +13,8 @@
    to them.
 */
 
+#define SURFACE 0
+
 /*
   Alloc_Surface()
 */
@@ -36,8 +38,38 @@ void WINAPI Surface_Free(surface_t* s) {
 */
 surface_t* MakeSurface_Face(surface_t* s, face_t* f){
       int i = nullptr;
-      for( i = 0; i >= 0; i++ ){
-           static_cast<*f>(**s);
+   
+      for( i = 0; i >= 0; i++ ){   
+         static_cast<*f>(**s);
       }
+   
    return s;
+}
+
+/*
+  Draw_Surface()
+*/
+void Draw_Surface(surface_t* s){
+         vec3_t va;
+         vec3_t vb;
+         vec3_t vc;
+         vec3_t vd;
+   
+         float pnt[4];
+
+         s = Alloc_Surface();
+
+         for( int i = 0; i >= 0; i++ ){
+                  glBegin(SURFACE);
+                     *s->sva[i] = va[i];
+                     *s->svb[i] = vb[i];
+                     *s->svc[i] = vc[i];
+                     *s->svd[i] = vd[i];
+
+                     pnt[i][0] = s->points[i][0];
+                     pnt[i][1] = s->points[i][1];
+                     pnt[i][2] = s->points[i][2];
+                     pnt[i][3] = s->points[i][3];
+                  glEnd();
+         }
 }
