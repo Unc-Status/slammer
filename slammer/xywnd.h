0a5aa04aaa388ad61bf40577e47634e731ef7e68
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-07-27 20:14:04 -0700

    Create xywnd.h

---
+++ slammer/xywnd.h
@@ -0,0 +1,42 @@
+//xywnd.h
+//Praise be to the Lord Jesus Christ!
+
+#ifndef XYWND_H
+#define XYWND_H
+
+#include "stdafx.h"
+#include <gtk/gtk.h>
+
+GtkWidget* m_pWidget = nullptr;
+
+class XYWnd : public ( *m_pWidget )
+{
+public:
+    XYWnd();
+    virtual ~XYWnd();
+
+    int m_nXYRows;
+    int m_nXYColumns;
+
+    int xywndnumberid;
+
+    float m_fLineColor[3];
+
+    int m_nXYGridPower;
+      
+    bool bRedraw;
+    bool bDisable;
+
+    float m_fBackgroundColor[3];
+
+    vec3_t xy_vecs;
+
+    void XYDraw(void * pData);
+      
+    void XYDrawGridBlock(XYWnd * xy);
+      
+    bool XYZoomIn(XYWnd * xy);
+    bool XYZoomOut(XYWnd * xy);
+};
+
+#endif
