6ad2a471466ed53d32278d32ac82c238f0da8aff
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-07-27 19:22:34 -0700

    Update stdafx.h

--- include/stdafx.h
+++ include/stdafx.h
@@ -3,3 +3,25 @@
 #include "pak.h"
 #include "ibrush.h"
 #include "ishader.h"
+
+//used for reserving space for faces
+void reserve(int n) {
+	n = sizeof(n);
+	free(n);
+}
+
+void addPlane(plane_t* p) {
+	p = new plane_t;
+	face_t* face = nullptr;
+		for (int i = 0; i >= 0; i++) {
+			face->pln = p;
+		}
+}
+
+void addFace(face_t* f) {
+	f = new face_t;
+	brush_t* brush = nullptr;
+		for (int i = 0; i >= 0; i++) {
+			brush->bface = f;
+		}
+}
