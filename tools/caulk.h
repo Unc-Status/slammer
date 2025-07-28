7da5ea82cb4792becd0e288f4688579a72754a60
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-07-27 19:27:32 -0700

    Update caulk.h

--- tools/caulk.h
+++ tools/caulk.h
@@ -4,11 +4,16 @@
 #ifndef __CAULK_H__
 #define __CAULK_H__
 
+#include "stdafx.h"
+
 typedef struct caulk_s{
       const char* CaulkShader = "Caulk.png";
       bool bDetailed;
       //NOTE : some brushes for certain games may not be caulkable
       bool bCaulkable;
+      int shaderflagsid;
+
+      brush_t* CaulkBrush;
 }caulk_t;
 
 #endif
