aa59afc89754cfddca3b46881efa509e284d843d
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-07-27 19:09:33 -0700

    Create traintoolz.h

---
+++ traintoolz/traintoolz.h
@@ -0,0 +1,28 @@
+//traintoolz.h
+//Praise be to the Lord Jesus Christ, he who believes in him shall not perish but have eternal life - john 3:16
+
+#ifndef __TRAINTOOLZ_H__
+#define __TRAINTOOLZ_H__
+
+#include "stdafx.h"
+
+class CTrainToolz
+{
+public:
+  CTrainToolz();
+  virtual ~CTrainToolz();
+
+  vec3_t position;
+
+  vec3_t x;
+  vec3_t y;
+  vec3_t z;
+
+  bool bSpawned;
+  bool bUniversal;
+
+  //load train
+  void LoadTrainModel(CTrainToolz* toolz, float pos[4], bool bSpwned, const char* mdlFile, epair_t * pKey, epair_t * pValue);
+}
+
+#endif
