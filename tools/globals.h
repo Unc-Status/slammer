55209737c86620339f34875cbfe41eca4c5c35e2
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-07-27 19:56:38 -0700

    Create globals.h

---
+++ tools/globals.h
@@ -0,0 +1,13 @@
+//globals.h
+//Praise to be the Lord Jesus Christ
+
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+typedef struct global_s{
+    bool m_bBrushSelectMode; 
+}global_t;
+
+global_t g_seglobals;
+
+#endif
