98e52239c87fe89040e68f21f877778148615520
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-07-27 20:15:43 -0700

    Update globals.h

--- tools/globals.h
+++ tools/globals.h
@@ -4,8 +4,14 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
+#include <gtk/gtk.h>
+
 typedef struct global_s{
     bool m_bBrushSelectMode; 
+
+    GtkWidget* g_pMainframe;
+    GtkWidget* g_pXYWnd;
+
 }global_t;
 
 global_t g_seglobals;
