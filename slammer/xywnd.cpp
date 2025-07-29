37ebf93dddc194990e348d49923e3a224c946bb2
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-07-28 18:54:35 -0700

    Update xywnd.cpp

--- slammer/xywnd.cpp
+++ slammer/xywnd.cpp
@@ -13,6 +13,26 @@ global_t* g_pWnd = nullptr;
 bool g_bRotateMode = false;
 bool g_bScaleMode = false;
 bool g_bSelectMode = false;
+bool g_bDragBrush = false;
+bool g_bSelectBrush = false;
+bool g_bDrawBrush = false;
+
+brush_t* pXYBrushes = nullptr;
+plane_t* pXYPlanes = nullptr;
+face_t* pXYFaces = nullptr;
+
+const char* g_cXYPrintTxt = nullptr;
+
+//Use GL
+void g_DrawXYIcon(const char* pXYIcon);
+
+const int g_nViewType = nullptr;
+
+int g_nXYPlaneNumber = 0;
+int g_nXYFaceNumber = 0;
+int g_nXYBrushNumber = 0;
+
+int g_nXYClipPoint = 0;
 
 XYWnd::XYWnd() : ( *g_pWnd->g_pXYWnd ){
 
