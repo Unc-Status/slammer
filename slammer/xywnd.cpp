8f321f6cbcbcd6e17e455a492026a7e182a95b81
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-07-28 18:42:25 -0700

    Update xywnd.cpp

--- slammer/xywnd.cpp
+++ slammer/xywnd.cpp
@@ -8,6 +8,12 @@ int g_nGridCallbackId = 0;
 
 global_t* g_pWnd = nullptr;
 
+#define PAGEFLIPS 2
+
+bool g_bRotateMode = false;
+bool g_bScaleMode = false;
+bool g_bSelectMode = false;
+
 XYWnd::XYWnd() : ( *g_pWnd->g_pXYWnd ){
 
 }
