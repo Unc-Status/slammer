331084d2d523ed5913aeb4e31d9c574d3767ceea
UncStatus++ <164366136+Unc-Status@users.noreply.github.com>
2025-07-29 15:03:55 -0500

    Create quaketypedesc.h

---
+++ gamecontrib/quake/quaketypedesc.h
@@ -0,0 +1,32 @@
+//quaketypedesc.h
+//Praise be to the Lord Jesus Christ my savior!
+
+#include "stdafx.h"
+
+#ifndef QUAKETYPEDESC_H
+#define QUAKETYPEDESC_H
+
+//version
+#define QUAKE_VERSION 
+
+//qtypes
+typedef plane_t qplane_t;
+typedef face_t qface_t;
+typedef brush_t qbrush_t;
+typedef entity_t qentity_y;
+typedef epair_t qepair_t;
+typedef global_t qglobal_t;
+typedef vec3_t qvec3_t;
+typedef vec_t qvec_t;
+typedef vec4_t qvec4_t;
+typedef surface_t qsurface_t;
+
+typedef char qchar;
+typedef bool qbool;
+
+void InitQuake( int );
+qbool bQuakeSel( qchar );//init game
+qchar QuakePath[2048];
+const qchar* QDirectories;
+
+#endif
